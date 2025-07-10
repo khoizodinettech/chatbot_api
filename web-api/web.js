10e8717ebebba28f196cbed15c22a541a329e29d
khoizodinettech <khoi.vu@zodinet.tech>
2025-07-10 14:36:59 +0700

    test

--- web-api/web.js
+++ web-api/web.js
@@ -1,11 +1,14 @@
 let Users = [];
+var firstName = document.getElementById("first_name").value;
+var lastName = document.getElementById("last_name").value;
+var email = document.getElementById("email").value;
+var avatar = document.getElementById("avatar").value;
 
 function ShowAllUsers() {
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET", "https://reqres.in/api/users", false);
   xhttp.setRequestHeader("x-api-key", "reqres-free-v1");
   xhttp.send();
-
   var data = JSON.parse(xhttp.responseText);
   var users = data.data;
   renderHTML(users);
@@ -36,16 +39,16 @@ function renderHTML(users) {
   tbody.innerHTML = html;
 }
 
-ShowAllUsers();
 function creatUser(event) {
   event.preventDefault();
-  var firstName = document.getElementById("first_name").value;
-  var lastName = document.getElementById("last_name").value;
-  var email = document.getElementById("email").value;
-  var avatar = document.getElementById("avatar").value;
+  firstName = document.getElementById("first_name").value;
+  lastName = document.getElementById("last_name").value;
+  email = document.getElementById("email").value;
+  avatar = document.getElementById("avatar").value;
+
   if (firstName && lastName && email) {
     var newUser = {
-      id: Users.length + 1,
+      id: Users[Users.length - 1].id + 1,
       first_name: firstName,
       last_name: lastName,
       email: email,
@@ -58,25 +61,35 @@ function creatUser(event) {
   }
   cleanUSers();
 }
-document.getElementById("form").onsubmit = creatUser;
+
 function deleteUser(id) {
   Users = Users.filter((user) => user.id !== id);
   renderHTML(Users);
 }
+
 function editUser(id) {
   var user = Users.find((user) => user.id === id);
+  var newFirtName = document.getElementById("first_name").value;
+  var newLastName = document.getElementById("last_name").value;
+  var newEmail = document.getElementById("email").value;
+  var newAvatar = document.getElementById("avatar").value;
   if (user) {
-    var newFirstName = prompt("Enter new first name:", user.first_name);
-    var newLastName = prompt("Enter new last name:", user.last_name);
-    var newEmail = prompt("Enter new email:", user.email);
-    if (newFirstName && newLastName && newEmail) {
-      user.first_name = newFirstName;
+    document.getElementById("first_name").value = user.first_name;
+    document.getElementById("last_name").value = user.last_name;
+    document.getElementById("email").value = user.email;
+    document.getElementById("avatar").value = user.avatar;
+    if(newFirtName && newLastName && newEmail && newAvatar) {
+      user.first_name = newFirtName;
       user.last_name = newLastName;
       user.email = newEmail;
-      user.avatar = prompt("Enter new avatar URL:", user.avatar);
+      user.avatar = newAvatar;
       renderHTML(Users);
+      cleanUSers();
     }
   } else {
     alert("User not found");
   }
 }
+
+ShowAllUsers();
+document.getElementById("form").onsubmit = creatUser;
